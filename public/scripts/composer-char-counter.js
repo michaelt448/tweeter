97203f009aef89e7116f001e09fbbad3c40223af
vagrant <mikegithub77@gmail.com>
2019-05-14 19:34:13 +0000

    added functionality to dynamically decrease the counter as more characters are added, the color will change when over the limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,48 @@
-$(document).ready(function(){
-  // $("textarea").change(function(){
-  //   console.log("The text has been changed.");
-  // });
+  $(document).ready(function(){
 
-  $("textarea").keydown(function(){
-    let currentVal = $(this).siblings(".counter").text();
-    let newVal = currentVal - 1;
-    console.log(newVal);
-    console.log($(this).siblings(".counter").text(newVal));
+    // $("textarea").keypress(function(){
+    //   let currentVal = $(this).siblings(".counter").text();
+    //   if(currentVal < 0) {
+    //     $(this).siblings(".counter").css("color", "red");
+    //   }
+    //   $(this).siblings(".counter").text(currentVal - 1);
+    // });
 
-  });
+    // pasteFunction() {
+    //  let currentVal = $(document).getElementById(".counter").text();
+    //  $(document).getElementById(".counter").text()
+    // }
 
-  // $("textarea").keyup(function() {
-  //   console.log("The key has been lifter");
-  // });
+    // WORKING COPY NOT REFACTORED
+    // const currentCount = Number($("textarea").siblings(".counter").text());
+    // $("textarea").oninput(function(){
+    //   const currentStringLength = $(this).val().length;
+    //   //console.log(currentStringLength);
+    //    if(currentCount < currentStringLength) {
+    //      $(this).siblings(".counter").css("color", "red");
+    //    }
+    //    $(this).siblings(".counter").text(currentCount - currentStringLength);
+    // });
 
-  // $("textarea").blur(function() {
-  //   console.log("There is a blur");
-  // });
 
-  // $("textarea").keypress(function() {
-  //   console.log(this);
-  // });
-});
+    //REFACTOR ATTEMPT
+      const currentCount = Number($("textarea").siblings(".counter").text());
+
+      $("textarea").keyup(function(){
+        update($(this));
+      });
+
+      const update = (currentTextObj) => {
+        const currentStringLength = currentTextObj.val().length;
+        colorText(currentCount,currentStringLength,currentTextObj.siblings(".counter"));
+        currentTextObj.siblings(".counter").text(currentCount - currentStringLength);
+      }
+
+      const colorText = (currentCount,currentStringLength,counter) => {
+         if(currentCount < currentStringLength) {
+           counter.css("color", "red");
+        } else {
+          counter.css("color", "black");
+        }
+      }
+    });
