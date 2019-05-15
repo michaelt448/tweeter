a600d595d1186f1207dae634508b54618d2f8cad
vagrant <mikegithub77@gmail.com>
2019-05-15 03:29:25 +0000

    refactored some of the code in the app.js fileto multiple functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,52 +4,96 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
  $(document).ready(function(){
- const createTweetElement = function(dataBase) {
-  const button = $("input");
-  button.click(function(e) {
-    e.preventDefault();
-    const textObj = $('textarea');
-    const textInput = textObj.val();
+ const createTweetElement = function(singleTweet) {
+  // const button = $("input");
+  // button.click(function(e) {
+  // e.preventDefault();
+  // const textObj = $('textarea');
+  // const textInput = textObj.val();
 
-    const newArticle = $('<article>').toggleClass('tweet');
-
-    const newTweeterHead = $('<header>').toggleClass('tweet-header');
-    const newTweeterHeadImg = $(`<img src = ${dataBase.user.avatars.small}>`).toggleClass("tweet-icon");
-    const newTweeterName = $('<h3>').toggleClass('name').text(dataBase.user.name);
-    const newTweeterUserName = $('<p>').toggleClass('username').text(dataBase.user.handle);
-    let fullHeader = newTweeterHead.append(newTweeterHeadImg).append(newTweeterName).append(newTweeterUserName);
-
-    const newTweeterParag = $(`<p> ${textInput}</p>`).toggleClass('tweet-text');
-
-    const newTweeterFooter = $('<footer>').toggleClass('tweet-footer');
-    const newTweeterFootParag = $('<p>').text(dataBase.created_at);
-    let fullFooter = newTweeterFooter.append(newTweeterFootParag);
-
-    let addParagraphToArticle = newArticle.append(fullHeader).append(newTweeterParag).append(fullFooter);
-    $('#tweet-container').append(addParagraphToArticle);
-  })
+  const newArticle = $('<article>').toggleClass('tweet');
+  const fullHeader = makeHeader(singleTweet);
+  const newTweeterParag = makeTweetParag(singleTweet.content.text);
+  const fullFooter = makeFooter(singleTweet);
+  return newArticle.append(fullHeader).append(newTweeterParag).append(fullFooter);
+// })
  }
 
+ const makeHeader =(singleTweet) => {
+    const newTweeterHead = $('<header>').toggleClass('tweet-header');
+    const newTweeterHeadImg = $(`<img src = ${singleTweet.user.avatars.small}>`).toggleClass("tweet-icon");
+    const newTweeterName = $('<h3>').toggleClass('name').text(singleTweet.user.name);
+    const newTweeterUserName = $('<p>').toggleClass('username').text(singleTweet.user.handle);
+    return newTweeterHead.append(newTweeterHeadImg).append(newTweeterName).append(newTweeterUserName);
+ }
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+ const makeTweetParag = (textInput) => {
+    return $('<p>').toggleClass('tweet-text').text(textInput);
+ }
+
+ const makeFooter = (singleTweet) => {
+    const newTweeterFooter = $('<footer>').toggleClass('tweet-footer');
+    const newTweeterFootParag = $('<p>').text(singleTweet.created_at);
+    return newTweeterFooter.append(newTweeterFootParag);
+ }
+
+  function renderTweets(tweets) {
+    for(tweet in tweets) {
+      let currentArticle = createTweetElement(tweets[tweet]);
+      console.log(currentArticle);
+      $('#tweet-container').append(currentArticle);
+      console.log($('#tweet-container').append(currentArticle));
+    }
+    // console.log($('#tweet-container'));
+  }
+
+// Fake data taken from tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
-var $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
 });
