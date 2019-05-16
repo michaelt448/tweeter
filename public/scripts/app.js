99ffd0d85f910f4a869bb6bd58e70a050ebf2b5d
vagrant <mikegithub77@gmail.com>
2019-05-16 18:44:04 +0000

    changed organization of the code, some refactoring to prevent possible future bugs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,95 +4,95 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
  $(document).ready(function(){
-   //const button = $("input");
- const createTweetElement = (singleTweet) => {
-  const newArticle = $('<article>').toggleClass('tweet');
 
-  const fullHeader = makeHeader(singleTweet);
-  const fullParagr = makeTweetParag(singleTweet.content.text);
-  const fullFooter = makeFooter(singleTweet);
+  // pete: I would use a more specific selector here, this could lead to bugs later
+  const button = $('input');
+  const textObj = $('textarea');
 
-  return newArticle.append(fullHeader, fullParagr, fullFooter);
- }
+  //Main function, all other functions start from here 
+  button.on('click',function(e) {
+    e.preventDefault();
+    $('.error').slideUp();
+    if(Number($('.counter').text()) < 0) {
+        $('.error').slideDown();
+        $('.error').text('You are over character limit');
+    }
+    else if(Number($('.counter').text()) === 140) {
+        $('.error').slideDown();
+        $('.error').append('Cannot submit empty message');
+    }
+    else {
+        $.ajax( {
+            type : 'POST',
+            url : '/tweets',
+            data : $('textarea').serialize() 
+        })
+        .then(function() {
+          console.log('successfully posted');
+          $('#tweet-container').empty();
+          dataLoaded(renderTweets);
+        })
+        $('textarea').val('');
+        $('textarea').focus();
+        $('.counter').text('140');
+    }
+ });
 
- const makeHeader = (singleTweet) => {
-    const newTweeterHead = $('<header>').toggleClass('tweet-header');
 
-    const newTweeterHeadImg = $(`<img src = ${singleTweet.user.avatars.small}>`).toggleClass("tweet-icon");
-    const newTweeterName = $('<h3>').toggleClass('name').text(singleTweet.user.name);
-    const newTweeterUserName = $('<p>').toggleClass('username').text(singleTweet.user.handle);
+ const dataLoaded = function(callback) {
+    // pete: if you use the success/error attributes for the object you pass to ajax you can handle errors right there
+  $.ajax({
+    type: 'GET',
+    url : '/tweets',
+    dataType : 'JSON'
+  })
+  .done(function(tweets) {
+     callback(tweets);
+  })
+};
 
-    return newTweeterHead.append(newTweeterHeadImg,newTweeterName,newTweeterUserName);
- }
-
- const makeTweetParag = (textInput) => {
-    return $('<p>').toggleClass('tweet-text').text(textInput);
- }
-
- const makeFooter = (singleTweet) => {
-    const newTweeterFooter = $('<footer>').toggleClass('tweet-footer');
-
-    const newTweeterFootParag = $('<p>').text(moment(singleTweet.created_at).fromNow());
-
-    return newTweeterFooter.append(newTweeterFootParag);
- }
-
-  function renderTweets(tweets) {
-    console.log(tweets);
-    for(let i = tweets.length - 1 ; i >= 0; i--) {
+function renderTweets(tweets) {
+  console.log(tweets);
+  for(let i = tweets.length - 1 ; i >= 0; i--) {
       console.log(tweets[i]);
       let currentArticle = createTweetElement(tweets[i]);
       $('#tweet-container').append(currentArticle);
-    }
   }
+};
 
-  const dataLoaded = function(callback) {
-    $.ajax({
-      type: 'GET',
-      url : '/tweets',
-      dataType : 'JSON'
-    })
-    .done(function(tweets) {
-       callback(tweets);
-    })
-  };
+// pete: this is indented incorrectly
+  const createTweetElement = (singleTweet) => {
+    // why use toggleClass when creating a tweet? just use addClass
+    const newArticle = $('<article>').addClass('tweet');
 
-  // button.on('click',function(e) {
-  //   $('#tweet-container').empty();
-  //   e.preventDefault();
-  //   dataLoaded();
-  //   // dataLoaded();
-  // })
+    const header = makeHeader(singleTweet);
+    const parag = makeTweetParag(singleTweet);
+    const footer = makeFooter(singleTweet);
 
+    return newArticle.append(header, parag, footer);
+ }
 
-  const button = $('input');
-  const textObj = $('textarea');
-  button.on('click',function(e) {
-     e.preventDefault();
-      $('.error').slideUp();
-      if($('.counter').text() < 0) {
-          $('.error').slideDown();
-          $('.error').text('You are over character limit');
-      }
-      else if($('.counter').text() == 140) {
-          $('.error').slideDown();
-          $('.error').append('Cannot submit empty message');
-      }
-      else {
-          $.ajax( {
-              type : 'POST',
-              url : '/tweets',
-              data : $('textarea').serialize() 
-          })
-          .then(function() {
-            console.log('successfully posted');
-            $('#tweet-container').empty();
-            // e.preventDefault();
-            dataLoaded(renderTweets);
-          })
-          $('textarea').val('');
-          $('textarea').focus();
-          $('.counter').text('140');
-      }
-  });
+ const makeHeader = (singleTweet) => {
+    const header = $('<header>').addClass('tweet-header');
+
+    // pete: the way you did this with templating is fine but a better choice might be using the .attr method
+    // also object destructuring would clean up a lot of these long property acess
+    const headImg = $(`<img src = ${singleTweet.user.avatars.small}>`).addClass("tweet-icon");
+    const name = $('<h3>').addClass('name').text(singleTweet.user.name);
+    const userName = $('<p>').addClass('username').text(singleTweet.user.handle);
+
+    return header.append(headImg, name, userName);
+ }
+
+ const makeTweetParag = (singleTweet) => {
+    return $('<p>').addClass('tweet-text').text(singleTweet.content.text);
+ }
+
+ const makeFooter = (singleTweet) => {
+    const footer = $('<footer>').addClass('tweet-footer');
+
+    const footParag = $('<p>').text(moment(singleTweet.created_at).fromNow());
+
+    return footer.append(footParag);
+ }
 });
